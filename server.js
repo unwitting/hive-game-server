afe1820c3aeb03fbc5bbda31e98f41e502650747
Jack Preston <unwttng@gmail.com>
2017-04-08 11:19:41 +0100

    Full flow

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use('*', (req, res, next) => {
 app.get('/game/free', (req, res) => {
   log(`_Free game_ request from player **${req.playerId}**`)
   log(`_Creating_ new RemotePlayer(**${req.playerId}**)`)
-  const remotePlayer = new RemotePlayer(req.playerId)
+  const remotePlayer = new RemotePlayer(req.playerId, { logFn: log })
   const otherWaitingPlayers = _(waitingPlayers).keys().reject(tok => waitingPlayers[tok].id === req.playerId).value()
   if (!otherWaitingPlayers.length) {
     const waitingToken = shortid.generate()
@@ -42,7 +42,57 @@ app.get('/game/free', (req, res) => {
     const game = new Game(whitePlayer, blackPlayer, { logFn: log })
     delete waitingPlayers[matchedPlayerToken]
     gamesInProgress[matchedPlayerToken] = game
+    game.begin()
     return res.send({ token: matchedPlayerToken })
+  }
+})
+
+app.get('/game/status/:token', (req, res) => {
+  const { token } = req.params
+  log(`_Game status_ request for token **${token}** from player **${req.playerId}**`)
+  if (waitingPlayers[token]) {
+    return res.send({ status: 'WAITING_FOR_SECOND_PLAYER' })
+  }
+  const game = gamesInProgress[token]
+  if (!game) {
+    return res.status(404).end()
+  }
+  return res.send({ status: 'IN_PROGRESS', state: game.state })
+})
+
+app.get('/game/status/:token/ack/:hash', (req, res) => {
+  const { token, hash } = req.params
+  log(`_Game state ack_ request for token **${token}** from player **${req.playerId}** with state hash **${hash}**`)
+  const game = gamesInProgress[token]
+  if (!game) {
+    return res.status(404).end()
+  }
+  const player = game.getPlayerById(req.playerId)
+  if (!player) {
+    return res.status(404).end()
+  }
+  if (player.acknowledgeStateByPlayer(game, hash)) {
+    return res.status(200).send()
+  } else {
+    return res.status(400).send({ status: 'HASH_MISMATCH' })
+  }
+})
+
+app.get('/game/:token/move/:move/:hash', (req, res) => {
+  const { token, move, hash } = req.params
+  log(`_Move_ request for token **${token}** from player **${req.playerId}** with move string **${move}**`)
+  const game = gamesInProgress[token]
+  if (!game) {
+    return res.status(404).end()
+  }
+  const player = game.getPlayerById(req.playerId)
+  if (!player) {
+    return res.status(404).end()
+  }
+  if (player.moveByPlayer(game, hash, move)) {
+    return res.status(200).send()
+  } else {
+    return res.status(400).send({ status: 'HASH_MISMATCH' })
   }
 })
 
